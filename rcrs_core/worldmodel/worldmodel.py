8cdbc0f027e24081d490e10e37502466a5b62be3
shima004 <yellow030408@gmail.com>
2024-10-20 00:35:29 +0900

    fix: circle import

--- rcrs_core/worldmodel/worldmodel.py
+++ rcrs_core/worldmodel/worldmodel.py
@@ -1,21 +1,17 @@
 from typing import List
-from rcrs_core.entities.entity import Entity
-from rcrs_core.entities import standardEntityFactory
-from rcrs_core.entities import human
-from rcrs_core.entities.area import Area
-from rcrs_core.entities.blockade import Blockade
-from rcrs_core.connection import URN
 
 from rtree import index
-import sys
 
+from rcrs_core.entities import standardEntityFactory
+from rcrs_core.entities.area import Area
+from rcrs_core.entities.blockade import Blockade
+from rcrs_core.entities.entity import Entity
 from rcrs_core.worldmodel.changeSet import ChangeSet
 from rcrs_core.worldmodel.entityID import EntityID
 
 
 class WorldModel:
     def __init__(self) -> None:
-
         self.index = index.Index()
         self.stored_types = {}
         self.unindexedـentities = {}
@@ -52,15 +48,18 @@ class WorldModel:
             existing_entity = self.get_entity(entity_id)
             added = False
             if existing_entity is None:
-                existing_entity = standardEntityFactory.StandardEntityFactory.make_entity(
-                    change_set.get_entity_urn(entity_id), entity_id.get_value())
+                existing_entity = (
+                    standardEntityFactory.StandardEntityFactory.make_entity(
+                        change_set.get_entity_urn(entity_id), entity_id.get_value()
+                    )
+                )
                 if existing_entity is None:
-                    print('world model merge existing entity is still None')
+                    print("world model merge existing entity is still None")
                     continue
                 added = True
 
             for property in change_set.get_changed_properties(entity_id):
-                existing_property = existing_entity.get_property(property.get_urn())   
+                existing_property = existing_entity.get_property(property.get_urn())
                 existing_property.take_value(property)
 
             if added:
@@ -76,10 +75,8 @@ class WorldModel:
             self.index.delete(human.entity_id, rectangle)
             left, bottom, right, top = self.make_rectangle(human)
             if left is not None:
-                self.index.insert(human.entity_id,
-                                  (left, bottom, right, top))
-                new_human_rectangles_to_push[human] = (
-                    left, bottom, right, top)
+                self.index.insert(human.entity_id, (left, bottom, right, top))
+                new_human_rectangles_to_push[human] = (left, bottom, right, top)
 
         for human in new_human_rectangles_to_push:
             rectangle = new_human_rectangles_to_push[human]
@@ -87,10 +84,10 @@ class WorldModel:
 
     def index_entities(self):
         if not self.indexed:
-            self.minx = float('inf')
-            self.miny = float('inf')
-            self.maxx = float('inf')
-            self.maxy = float('inf')
+            self.minx = float("inf")
+            self.miny = float("inf")
+            self.maxx = float("inf")
+            self.maxy = float("inf")
 
             self.index = index.Index()
             self.human_rectangles.clear()
@@ -98,21 +95,19 @@ class WorldModel:
             for entity in self.unindexedـentities.values():
                 left, bottom, right, top = self.make_rectangle(entity)
                 if left is not None:
-                    self.index.insert(entity.entity_id,
-                                      (left, bottom, right, top))
+                    self.index.insert(entity.entity_id, (left, bottom, right, top))
                     self.minx = min(self.minx, left, right)
                     self.maxx = max(self.maxx, left, right)
                     self.miny = min(self.miny, bottom, top)
                     self.maxy = max(self.maxy, bottom, top)
                     if isinstance(entity, Human):
-                        self.human_rectangles[entity] = (
-                            left, bottom, right, top)
+                        self.human_rectangles[entity] = (left, bottom, right, top)
             self.indexed = True
 
     def make_rectangle(self, entity):
-        x1 = x2 = y1 = y2 = float('inf')
+        x1 = x2 = y1 = y2 = float("inf")
         apexes = []
-        
+
         if isinstance(entity, Area):
             apexes = entity.apexList
         elif isinstance(entity, Blockade):
@@ -126,14 +121,14 @@ class WorldModel:
             return None, None, None, None
 
         if len(apexes) == 0:
-            print('this ' + str(type(entity)) + 'entity does not have apexes!!')
+            print("this " + str(type(entity)) + "entity does not have apexes!!")
             return None, None, None, None
 
         for i in range(0, len(apexes), 2):
             x1 = min(x1, apexes[i])
             x2 = max(x2, apexes[i])
-            y1 = min(y1, apexes[i+1])
-            y2 = max(y2, apexes[i+1])
+            y1 = min(y1, apexes[i + 1])
+            y2 = max(y2, apexes[i + 1])
         return x1, y1, x2, y2
 
     # returns rect(x, y, w, h)
@@ -153,9 +148,9 @@ class WorldModel:
     def get_objects_in_rectangle(self, x1, y1, x2, y2):
         if self.indexed == False:
             self.index_entities()
-        
+
         entities_ids = index.intersection(x1, y1, x2, y2)
-        
+
         result = []
         for entity_id in entities_ids:
             result.append(self.get_entity(entity_id))
@@ -164,11 +159,11 @@ class WorldModel:
 
     def get_objects_in_range(self, entity, range):
         location = entity.get_location()
-    
-        if (location == None):
+
+        if location == None:
             return []
 
         x = location[0]
         y = location[1]
-        
+
         return self.get_objects_in_rectangle(x - range, y - range, x + range, y + range)
