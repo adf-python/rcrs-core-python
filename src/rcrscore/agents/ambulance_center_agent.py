7274db709c54a2adf4f57cd0d0c7899dfbf34d16
shima004 <yellow030408@gmail.com>
2025-08-02 19:57:18 +0900

    feat: 新しいエージェントクラスを追加し、各エージェントの基本機能を実装

---
+++ src/rcrscore/agents/ambulance_center_agent.py
@@ -0,0 +1,32 @@
+from logging import Logger
+
+from rcrscore.agents.agent import Agent
+from rcrscore.logger.logger import get_logger
+from rcrscore.proto.RCRSProto_pb2 import MessageListProto
+from rcrscore.urn.entity import EntityURN
+from rcrscore.worldmodel.change_set import ChangeSet
+
+
+class AmbulanceCenterAgent(Agent):
+  def __init__(self, precompute_flag: bool = False):
+    super().__init__(precompute_flag)
+
+  def precompute(self) -> None:
+    return
+
+  def think(self, time: int, change_set: ChangeSet, hear: MessageListProto) -> None:
+    return
+
+  def post_connect(self) -> None:
+    return
+
+  def get_name(self) -> str:
+    return "AmbulanceCenterAgent"
+
+  def get_logger(self) -> Logger:
+    if not self.logger:
+      self.logger = get_logger(self.get_name(), self.get_entity_id().get_value())
+    return self.logger
+
+  def get_urn(self) -> EntityURN:
+    return EntityURN.AMBULANCE_CENTER
---
+++ src/rcrscore/agents/ambulance_team_agent.py
@@ -0,0 +1,50 @@
+from random import Random
+
+from rcrscore.agents.agent import Agent
+from rcrscore.entities.area import Area
+from rcrscore.entities.entity_id import EntityID
+from rcrscore.proto.RCRSProto_pb2 import MessageListProto
+from rcrscore.urn.entity import EntityURN
+from rcrscore.worldmodel.change_set import ChangeSet
+
+
+class AmbulanceTeamAgent(Agent):
+  def __init__(self, precompute_flag: bool = False):
+    super().__init__(precompute_flag)
+    self.random = Random(0)
+
+  def precompute(self) -> None:
+    return
+
+  def think(self, time: int, change_set: ChangeSet, hear: MessageListProto) -> None:
+    self.get_logger().info(f"{type(hear)}: {hear}")
+
+    next_location = self.random_walk()
+    if next_location:
+      self.get_logger().info(f"Time step: {time}, Moving to: {next_location}")
+      self.send_move(time, [next_location])
+    else:
+      self.get_logger().info(f"Time step: {time}, No move, resting.")
+      self.send_rest(time)
+
+  def random_walk(self) -> EntityID | None:
+    if not (location := self.location()):
+      return None
+
+    if not isinstance(location, Area):
+      return None
+
+    neighbors: set[EntityID] = location.get_neighbors()
+    if not neighbors:
+      return None
+
+    return self.random.choice(list(neighbors))
+
+  def post_connect(self) -> None:
+    return
+
+  def get_name(self) -> str:
+    return "AmbulanceTeamAgent"
+
+  def get_urn(self) -> EntityURN:
+    return EntityURN.AMBULANCE_TEAM
